4e7feaab40594c477ebc97fcec6d2b1c64fb0a67
toothless <toothless042@gmail.com>
2022-11-02 00:35:19 +0900

    Lecture 07 Context

---
+++ Lecture07/my-app/src/DarkOrLight.jsx
@@ -0,0 +1,20 @@
+import { useState, useCallback } from "react";
+import ThemeContext from "./ThemeContext";
+import MainContext from "./MainContext";
+
+function DarkOrLight(props){
+    const [theme, setTheme] = useState("light");
+
+    const toggleTheme = useCallback(() => {
+        if(theme == "light") setTheme("dark");
+        else if(theme == "dark") setTheme("light");
+    }, [theme]);
+
+    return (
+        <ThemeContext.Provider value={{theme, toggleTheme}}>
+            <MainContext></MainContext>
+        </ThemeContext.Provider>
+    )
+}
+
+export default DarkOrLight;
