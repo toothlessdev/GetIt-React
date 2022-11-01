63ab073fa5afea77ba8ea4e5aac08d21c90d095d
toothless <toothless042@gmail.com>
2022-11-02 00:21:31 +0900

    Lecture 06 Inheritance

---
+++ Lecture06/my-app/src/ProfileCard.jsx
@@ -0,0 +1,12 @@
+import Card from "./Card";
+
+function ProfileCard(props){
+    return (
+        <Card title="Kim Dae Geon" backgroundColor="yellow">
+            <p>안녕하세요, 대건이입니다~</p>
+            <p>저는 리액트를 학습중에 있습니다.</p>
+        </Card>
+    )
+}
+
+export default ProfileCard;
